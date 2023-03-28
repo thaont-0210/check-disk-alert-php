e4877671a3c90d3219a1a89174ce161e698ee90a
Nguyen The Thao <nguyen.the.thao@sun-asterisk.com>
2023-03-28 11:00:16 +0700

    Add .env example

--- index.php
+++ index.php
@@ -1,108 +1,108 @@
 <?php
 
 class Disk {
-	public $diskOver = 70; // percent
-	public $dfCmdResult = array();
-	public $diskResult = array();
-	public $dfCmdOverResult = array();
-	public $diskOverResult = array();
+    public $diskOver = 70; // percent
+    public $dfCmdResult = array();
+    public $diskResult = array();
+    public $dfCmdOverResult = array();
+    public $diskOverResult = array();
 
-	public function run()
-	{
-		// $this->runDiskHumanReadableCmd();
-		// $this->getDiskHumanReadable();
-		$this->runDiskHumanReadableOverCmd();
-		$this->getDiskHumanReadableOver();
-	}
+    public function run()
+    {
+        // $this->runDiskHumanReadableCmd();
+        // $this->getDiskHumanReadable();
+        $this->runDiskHumanReadableOverCmd();
+        $this->getDiskHumanReadableOver();
+    }
 
-	public function setDiskOverPercent($percent)
-	{
-		$this->diskOver = $percent;
-	}
+    public function setDiskOverPercent($percent)
+    {
+        $this->diskOver = $percent;
+    }
 
-	public function runDiskHumanReadableCmd()
-	{
-		exec('df -h', $output, $retval);
+    public function runDiskHumanReadableCmd()
+    {
+        exec('df -h', $output, $retval);
 
-		$this->dfCmdResult = array(
-			'code' => $retval,
-			'result' => $output,
-		);
+        $this->dfCmdResult = array(
+            'code' => $retval,
+            'result' => $output,
+        );
 
-		return 0;
-	}
+        return 0;
+    }
 
-	public function runDiskHumanReadableOverCmd()
-	{
-		exec("df -h | awk '$5 > {$this->diskOver}' | { read -r line; sort -k5; }", $output, $retval);
+    public function runDiskHumanReadableOverCmd()
+    {
+        exec("df -h | awk '$5 > {$this->diskOver}' | { read -r line; sort -k5; }", $output, $retval);
 
-		$this->dfCmdOverResult = array(
-			'code' => $retval,
-			'result' => $output,
-		);
+        $this->dfCmdOverResult = array(
+            'code' => $retval,
+            'result' => $output,
+        );
 
-		return 0;
-	}
+        return 0;
+    }
 
-	public function getDiskHumanReadable()
-	{
-		$result = array();
-		if ($this->dfCmdResult['code'] == 0) {
-			for ($i = 0; $i < count($this->dfCmdResult['result']); $i++) {
-				$result[$i] = processStringToArrayHelper($this->dfCmdResult['result'][$i]);
-			}
-		}
+    public function getDiskHumanReadable()
+    {
+        $result = array();
+        if ($this->dfCmdResult['code'] == 0) {
+            for ($i = 0; $i < count($this->dfCmdResult['result']); $i++) {
+                $result[$i] = processStringToArrayHelper($this->dfCmdResult['result'][$i]);
+            }
+        }
 
-		$this->diskResult = $result;
+        $this->diskResult = $result;
 
-		return 0;
-	}
+        return 0;
+    }
 
-	public function getDiskHumanReadableOver()
-	{
-		$result = array();
-		if ($this->dfCmdOverResult['code'] == 0 && count($this->dfCmdOverResult['result']) > 0) {
-			for ($i = count($this->dfCmdOverResult['result']) - 1; $i >= 0; $i--) {
-				$result[$i] = processStringToArrayHelper($this->dfCmdOverResult['result'][$i]);
-			}
-		}
+    public function getDiskHumanReadableOver()
+    {
+        $result = array();
+        if ($this->dfCmdOverResult['code'] == 0 && count($this->dfCmdOverResult['result']) > 0) {
+            for ($i = count($this->dfCmdOverResult['result']) - 1; $i >= 0; $i--) {
+                $result[$i] = processStringToArrayHelper($this->dfCmdOverResult['result'][$i]);
+            }
+        }
 
-		$this->diskOverResult = $result;
+        $this->diskOverResult = $result;
 
-		return 0;
-	}
+        return 0;
+    }
 }
 
 
 class Slack
 {
-	public $postMessageUrl = "https://slack.com/api/chat.postMessage";
+    public $postMessageUrl = "https://slack.com/api/chat.postMessage";
 
-	function sendMessage($message, $channel)
+    function sendMessage($message, $channel)
     {
         $ch = curl_init($this->postMessageUrl);
-	    $data = http_build_query([
-	        "token" => "xxx",
-	    	"channel" => $channel,
-	    	"text" => $message,
-	    ]);
+        $data = http_build_query([
+            "token" => "xxx",
+            "channel" => $channel,
+            "text" => $message,
+        ]);
 
-	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-	    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	    $result = curl_exec($ch);
-	    curl_close($ch);
-	    
-	    return $result;
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        
+        return $result;
     }
 }
 
 function processStringToArrayHelper($string, $regex = '!\s+!')
 {
-	$string = preg_replace($regex, ' ', $string);
+    $string = preg_replace($regex, ' ', $string);
 
-	return explode(' ', $string);
+    return explode(' ', $string);
 }
 
 class DotEnv
